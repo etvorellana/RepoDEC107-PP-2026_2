a7f1bdca525c8e6abf2fdc8d8dcafe62e1a5cb70
etvorellana <evalero@uesc.br>
2026-08-27 06:18:48 -0300

    Exemplos 01 de OpenMP

---
+++ Aula_001-OpenMP/par_omp_02.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(void)
+{
+    int n, nth;
+    float start, stop;
+
+    #ifdef _OPENMP //Compilação condicional
+        n = omp_get_max_threads();      //função OpenMP
+        printf("Número de threads disponíveis: %d\n", n);
+        n = omp_get_num_threads();      // Função OpenMP
+        nth = omp_get_thread_num(); // Função OpenMP
+    #else
+        n = 1;
+        nth = 0;
+    #endif
+    printf("Número de threads em execução: %d\n", n);
+    printf("Executando na thread: %d\n", nth);
+
+    stop = start = omp_get_wtime(); // Função OpenMP
+    #pragma omp parallel
+    {
+        int n, nth;
+        #ifdef _OPENMP //Compilação condicional
+            n = omp_get_num_threads();      // Função OpenMP
+            nth = omp_get_thread_num(); // Função OpenMP
+        #else
+            n = 1;
+            nth = 0;
+        #endif
+        while(TRUE)
+        {
+            if (nth == 0) {
+                stop = omp_get_wtime(); // Função OpenMP
+            }
+            if (stop - start > 5.0 + (16 - nth)) {
+                break;
+            }
+        }
+        printf("Número de threads em execução: %d\n", n);
+        printf("Executando na thread: %d\n", nth);
+    }
+    return 0;
+
+}
+
+// compile with: gcc -fopenmp par_omp_02.c -o main_02
+// run with: ./main_02
+// compile with: gcc par_omp_02.c -o main_02_
+// run with: ./main_02_
